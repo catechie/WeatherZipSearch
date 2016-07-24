91414b591c7c29f620c0f334c957ef54ee713db4
catechie <catechie@gmail.com>
2016-07-23 23:30:31 -0500

    Update MainActivity.java

--- app/src/main/java/com/booba/weatherzipsearch/MainActivity.java
+++ app/src/main/java/com/booba/weatherzipsearch/MainActivity.java
@@ -183,7 +183,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private class ProcessWeatherHttpRequestTask extends AsyncTask<String, Void, String> {
+    private static class ProcessWeatherHttpRequestTask extends AsyncTask<String, Void, String> {
         String url = "";
         String zip;
         @Override
